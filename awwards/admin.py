36df186b137aa0a194974d26e566a58f37c089fc
ClintonClin <clintonclin00@gmail.com>
2019-01-07 05:33:01 +0300

    added functions in my admin.py that an admin can run

---
+++ awwards/admin.py
@@ -0,0 +1,16 @@
+from .models import categories, technologies, colors, countries, Project, Profile, Rating
+from django.contrib import admin
+
+
+class ProjectAdmin(admin.ModelAdmin):
+    filter_horizontal = ('technologies', 'categories', 'colors')
+
+
+# Register your models here.
+admin.site.register(categories)
+admin.site.register(technologies)
+admin.site.register(colors)
+admin.site.register(countries)
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(Profile)
+admin.site.register(Rating)
